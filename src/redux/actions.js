804fdb784c9d4769fa6e1f04116887d4b1b291f6
ZOVUT YAKOV <108018683+Yakov29@users.noreply.github.com>
2025-10-14 12:53:35 +0300

    .

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,23 @@
-export const addName = (payload) => ({ type: "names/add", payload }); 
-export const removeName = (index) => ({ type: "names/remove", payload: index });
-export const setFilter = (filter) => ({ type: "names/setFilter", payload: filter });
+// export const addName = (payload) => ({ type: "names/add", payload }); 
+// export const removeName = (index) => ({ type: "names/remove", payload: index });
+// export const setFilter = (filter) => ({ type: "names/setFilter", payload: filter });
+
+import { createAction } from "@reduxjs/toolkit";
+
+
+
+export const addName = createAction('names/add', (payload) => {
+    return {
+        payload
+    }
+})
+export const removeName = createAction('names/remove', (index) => {
+    return {
+        payload: index,
+    }
+})
+export const setFilter = createAction('names/setFilter', (filter) => {
+    return {
+        payload: filter
+    }
+})
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,36 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { addName, removeName, setFilter } from "./actions";
+
 const initialState = {
   list: [],
-  filter: ""
+  filter: "",
 };
 
-export const namesReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "names/add":
+// export const namesReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "names/add":
+//       return { ...state, list: [...state.list, action.payload] };
+//     case "names/remove":
+//       return { ...state, list: state.list.filter((_, i) => i !== action.payload) };
+//     case "names/setFilter":
+//       return { ...state, filter: action.payload };
+//     default:
+//       return state;
+//   }
+// };
+
+export const namesReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addName, (state, action) => {
       return { ...state, list: [...state.list, action.payload] };
-    case "names/remove":
-      return { ...state, list: state.list.filter((_, i) => i !== action.payload) };
-    case "names/setFilter":
+    })
+    .addCase(removeName, (state, action) => {
+      return {
+        ...state,
+        list: state.list.filter((_, i) => i !== action.payload),
+      };
+    })
+    .addCase(setFilter, (state, action) => {
       return { ...state, filter: action.payload };
-    default:
-      return state;
-  }
-};
+    });
+});
