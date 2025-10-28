1ca97423b5cac9310118d7d8a5be81a30fdcff96
ZOVUT YAKOV <108018683+Yakov29@users.noreply.github.com>
2025-10-28 10:53:59 +0200

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import namesReducer from "./reducer";
 
+import storage from "redux-persist/lib/storage";
+
+import { persistStore, persistReducer } from "redux-persist";
+
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+
+export const persistedReducer = persistReducer(persistConfig, namesReducer)
+
 export const store = configureStore({
   reducer: {
-    names: namesReducer,
+    names: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
 });
+
+
+export const persistor = persistStore(store)
